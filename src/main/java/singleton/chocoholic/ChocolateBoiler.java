c54272ae3e36a59a6e812dac7ae11f69124598d3
Noman5237 <anonyman637@gmail.com>
2022-05-18 13:25:39 +0600

    Implemented double-checked locking for thread safety

--- src/main/java/singleton/chocoholic/ChocolateBoiler.java
+++ src/main/java/singleton/chocoholic/ChocolateBoiler.java
@@ -4,17 +4,21 @@ public class ChocolateBoiler {
 	
 	private boolean empty;
 	private boolean boiled;
-	private static ChocolateBoiler uniqueInstance;
+	private volatile static ChocolateBoiler uniqueInstance;
 	
 	private ChocolateBoiler() {
 		empty = true;
 		boiled = false;
 	}
 	
-	public static synchronized ChocolateBoiler getInstance() {
+	public static ChocolateBoiler getInstance() {
 		if (uniqueInstance == null) {
-			System.out.println("Creating unique instance of Chocolate Boiler");
-			uniqueInstance = new ChocolateBoiler();
+			synchronized (ChocolateBoiler.class) {
+				if (uniqueInstance == null) {
+					System.out.println("Creating unique instance of Chocolate Boiler");
+					uniqueInstance = new ChocolateBoiler();
+				}
+			}
 		}
 		System.out.println("Returning instance of Chocolate Boiler");
 		return uniqueInstance;
